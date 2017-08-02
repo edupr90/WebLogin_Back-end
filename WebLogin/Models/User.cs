6d93360dfaa173ae6f675756b85f20c05e1e21b1
Eduardo Velez <evelez1990@hotmail.com>
2017-08-01 22:15:05 -0400

    Inittial Commit

---
+++ WebLogin/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebLogin.Models
+{
+    public static class User
+    {
+        //Variables que se van a recopilar de la bases de datos.
+    }
+}
